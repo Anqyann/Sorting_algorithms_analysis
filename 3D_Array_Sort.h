7373e39fb293f830d70afc7d0ee1cb28da944fc5
Anna Idolova <anna.idolova@gmail.com>
2025-06-11 16:51:21 +0300

    3D_Array_Sort.h

--- 3D_Array_Sort.h
+++ 3D_Array_Sort.h
@@ -1,7 +1,7 @@
 #ifndef _ARRAY_3D_SORT_H_INCLUDED
 #define _ARRAY_3D_SORT_H_INCLUDED
 #include <time.h>
-clock_t  Select_6_3DArr();//сортування №6 методу прямого вибору для тривимірного масиву
-clock_t Select_1_Exchange_3DArr();//сортування "вибір№1 – обмін" для тривимірного масиву
-clock_t Shell_2_3DArr();//сортування №2 Шелла для тривимірного масиву
+clock_t  Select_6_3DArr();//sorting of the direct selection method for a three-dimensional array
+clock_t Select_1_Exchange_3DArr();//sorting "selection - exchange" for a three-dimensional array
+clock_t Shell_2_3DArr();//Shell's sorting for a three-dimensional array
 #endif // _ARRAY_3D_SORT_H_INCLUDED
