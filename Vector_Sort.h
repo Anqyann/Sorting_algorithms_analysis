dfa5057f140318242dbc12d28c6d5f240da63797
Anna Idolova <anna.idolova@gmail.com>
2025-06-11 17:18:58 +0300

    Update Vector_Sort.h

--- Vector_Sort.h
+++ Vector_Sort.h
@@ -1,7 +1,7 @@
 #ifndef VECTOR_SORT_H_INCLUDED
 #define VECTOR_SORT_H_INCLUDED
 #include <time.h>
-clock_t Select_6(); //сортування №6 методу прямого вибору для вектора
-clock_t Select_1_Exchange(); //сортування "вибір№1 – обмін" для вектора
-clock_t Shell_2(); //сортування №2 Шелла для вектора
+clock_t Select_6(); //sorting of the direct selection method for a vector
+clock_t Select_1_Exchange(); //sorting "choice - exchange" for vector
+clock_t Shell_2(); //Shell's sorting  for vector
 #endif // VECTOR_SORT_H_INCLUDED
