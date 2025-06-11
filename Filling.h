0636003d46fc60be0092ab02b9b007003482962a
Anna Idolova <anna.idolova@gmail.com>
2025-06-11 16:54:22 +0300

    Update Filling.h

--- Filling.h
+++ Filling.h
@@ -1,9 +1,9 @@
 #ifndef INPUT_AND_OUTPUT_H_INCLUDED
 #define INPUT_AND_OUTPUT_H_INCLUDED
-void ordered_filling_vec();//впорядковане заповнення вектора
-void unordered_filling_vec(); //невпорядковане заповнення вектора
-void inversely_ordered_filling_vec(); //обернено впорядковане заповнення вектора
-void ordered_filling_3d_array();  //впорядковане заповнення тривимвірного масиву
-void unordered_filling_3d_array(); //невпорядковане заповнення тривимвірного масиву
-void inversely_ordered_filling_3d_array(); //обернено впорядковане заповнення тривимвірного масиву
+void ordered_filling_vec();//ordered filling of the vector
+void unordered_filling_vec(); //disordered filling of the vector
+void inversely_ordered_filling_vec(); //reverse ordered filling of the vector
+void ordered_filling_3d_array();  //ordered filling of the three-dimensional array
+void unordered_filling_3d_array();//disordered filling of the three-dimensional array
+void inversely_ordered_filling_3d_array(); //reverse ordered filling of the three-dimensional array
 #endif // INPUT_AND_OUTPUT_H_INCLUDED
