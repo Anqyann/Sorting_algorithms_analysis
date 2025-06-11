a342afb7e86b94dc1379326c8f00aee153396419
Anna Idolova <anna.idolova@gmail.com>
2025-06-11 17:09:46 +0300

    Update Menu.h

--- Menu.h
+++ Menu.h
@@ -1,12 +1,12 @@
 #ifndef MENU_H_INCLUDED
 #define MENU_H_INCLUDED
-void menu();//головне меню
-//розміри масиву
-#define P 8000 //кількість перерізів
-#define M 400 //кількість рядків
-#define N 2 //кількість стовпців
-int Vec[P]; //оголошення вектора довжиною P
-int ***Arr3D; //оголошення тривимірного масиву
-void Arr3D_declaration(); //функція оголошення динамічного тривимірного масиву
-void Arr3D_free(); //функція для звільнення пам'яті
+void menu();//main menu
+//array dimensions
+#define P 8000 //number of sections
+#define M 400 //number of rows
+#define N 2 //number of columns
+int Vec[P]; //declaration of a vector of length P
+int ***Arr3D; //declaration of a three-dimensional array
+void Arr3D_declaration(); //function for declaring a dynamic three-dimensional array
+void Arr3D_free(); //function for freeing memory
 #endif // MENU_H_INCLUDED
